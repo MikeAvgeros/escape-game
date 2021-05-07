2090dffd83fb4111f879d6ac42733d13c2f66e1e
Mike Avgeros <mike.avgeros@gmail.com>
2021-05-07 14:57:49 +0100

    Updated the showRoom functionality. Added a second room to test the progression.

--- assets/js/gamemanager.js
+++ assets/js/gamemanager.js
@@ -4,7 +4,8 @@ let roomIndex = 0;
 let storyIndex = 0;
 
 const images = {
-    roomOne: "../escape-game/assets/img/roomOne.jpg"
+    roomOne: "../escape-game/assets/img/graypaintedroom.jpg",
+    roomTwo: "../escape-game/assets/img/skullroom.jpg",
 };
 
 const storyNodes = {
@@ -66,9 +67,7 @@ const storyNodes = {
             text: "You see a rusty key in the corner of the room.",
             actions: [
                 {
-                    text: "Use Key",
-                    response: "You have successfully opened the door with the key"
-                    // play a happy melody and progress to room 2
+                    text: "Use Key"
                 },
                 {
                     text: "Smash Door",
@@ -76,24 +75,120 @@ const storyNodes = {
                     // reduce the life points from the player
                 }
             ]
+        },
+        {
+            id: 6,
+            text: "You have successfully opened the door with the key",
+            actions: [
+                {
+                    text: ""
+                },
+                {
+                    text: ""
+                }
+            ]
+        }
+    ],
+    2: [
+        {
+            id: 1,
+            text: "You see a room full of skulls",
+            actions: [
+                {
+                    text: "Examine Room",
+                },
+                {
+                    text: "Go Back",
+                    response: "The door behind you closes"
+                }
+            ]
+        },
+        {
+            id: 2,
+            text: "You notice some broken glass on the floor next to you.",
+            actions: [
+                {
+                    text: "Take glass",
+                },
+                {
+                    text: "Do nothing",
+                    response: "Doing nothing won't help you here. You need to escape"
+                }
+            ]
+        },
+        {
+            id: 3,
+            text: "Your body falls on the floor and your hand reaches the broken glass",
+            actions: [
+                {
+                    text: "Cut Rope",
+                },
+                {
+                    text: "Kill Yourself",
+                    response: "You have died"
+                }
+            ]
+        },
+        {
+            id: 4,
+            text: "You manage to cut the rope and break free. You see a door to your right.",
+            actions: [
+                {
+                    text: "Open Door",
+                    response: "The door appears to be locked."
+                },
+                {
+                    text: "Examine Room",
+                }
+            ]
+        },
+        {
+            id: 5,
+            text: "You see a rusty key in the corner of the room.",
+            actions: [
+                {
+                    text: "Use Key"
+                },
+                {
+                    text: "Smash Door",
+                    response: "You try to smash the door but it doesn't work and you hurt yourself."
+                    // reduce the life points from the player
+                }
+            ]
+        },
+        {
+            id: 6,
+            text: "You have successfully opened the door with the key",
+            actions: [
+                {
+                    text: ""
+                },
+                {
+                    text: ""
+                }
+            ]
         }
     ]
 };
 
 const items = {
-    roomOne: new Item("key", "img")
+    roomOne: new Item("key", "img"),
+    roomTwo: new Item("sword", "img")
 };
 
 const lootItems = {
-    roomOne: new Item("name", "img")
+    roomOne: new Item("name", "img"),
+    roomTwo: new Item("name", "img")
 }
 
 const enemies = {
-    roomOne: new Enemy("enemyOne", "health", "attack", "defense", lootItems.roomOne)
+    roomOne: new Enemy("enemyOne", "health", "attack", "defense", lootItems.roomOne),
+    roomTwo: new Enemy("enemyOne", "health", "attack", "defense", lootItems.roomOne)
 };
 
 const rooms = [
-    roomOne = new Room("Room One", 1, images.roomOne, storyNodes.roomOne, items.roomOne, enemies.roomOne)
+    roomOne = new Room("Gray Room", 1, images.roomOne, storyNodes.roomOne, items.roomOne, enemies.roomOne),
+    roomTwo = new Room("Skull Room", 2, images.roomTwo, storyNodes.roomTwo, items.roomTwo, enemies.roomTwo)
 ];
 
 function showRoom(roomIndex) {
@@ -108,12 +203,21 @@ function showStory(roomIndex, storyIndex) {
     const storyNode = storyNodes[currentRoom.id].find(storyNode => storyNode.id === storyIndex);
     textContainer.innerHTML = `<p>${storyNode.text}</p>`;
     const actions = storyNode.actions;
-    for (let i=0; i<buttons.length; i++) {
-        buttons[i].innerText = actions[i].text;
-        buttons[i].addEventListener("click", () => {
-            storyNode.actions[i].hasOwnProperty("response") ? 
-            textContainer.innerHTML = `<p>${storyNode.actions[i].response}</p>` : showStory(roomIndex, storyIndex);
-        })
+    if (storyIndex < storyNodes[currentRoom.id].length) {
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].innerText = actions[i].text;
+            buttons[i].addEventListener("click", () => {
+                if (storyNode.actions[i].hasOwnProperty("response")) {
+                    textContainer.innerHTML = `<p>${storyNode.actions[i].response}</p>` 
+                }
+                else {
+                    showStory(roomIndex, storyIndex);
+                }
+            });
+        }
+    }
+    else {
+        nextRoom();
     }
 }
 
@@ -123,8 +227,13 @@ function startGame() {
 }
 
 function nextRoom() {
-    roomIndex++;
-    showStory(roomIndex, storyIndex); 
+    setTimeout(function() {
+        console.log("called");
+        roomIndex++;
+        storyIndex = 0;
+        showRoom(roomIndex);
+        showStory(roomIndex, storyIndex); 
+    }, 1000);
 }
 
 startGame();
