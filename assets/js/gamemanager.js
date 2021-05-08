3327e3042436be8995bf70457b499b185ee5cc90
Mike Avgeros <mike.avgeros@gmail.com>
2021-05-08 18:07:31 +0100

    Added a fade-in effect on the buttons before they load new actions.

--- assets/js/gamemanager.js
+++ assets/js/gamemanager.js
@@ -7,8 +7,8 @@ let storyIndex = 0;
 const player = new Player("Mike", 100, 100, 100);
 
 const items = {
-    key: new Item("key", "img"),
-    glass: new Item("glass", "img")
+    item1: new Item("key", "img"),
+    item2: new Item("glass", "img")
 };
 
 const enemies = {
@@ -16,13 +16,13 @@ const enemies = {
 }
 
 const images = {
-    roomOne: "../assets/img/graypaintedroom.jpg",
-    roomTwo: "../assets/img/skullroom.jpg",
+    room1: "../assets/img/graypaintedroom.jpg",
+    room2: "../assets/img/skullroom.jpg",
 };
 
 const rooms = [
-    roomOne = new Room("Gray Room", images.roomOne, 1),
-    roomTwo = new Room("Skull Room", images.roomTwo, 2)
+    room1 = new Room("Gray Room", images.room1, 1),
+    room2 = new Room("Skull Room", images.room2, 2)
 ];
 
 const storyNodes = getStoryNodes(player, items, enemies);
@@ -32,7 +32,7 @@ function getStoryNodes(player, items, enemies) {
         1: [
             {
                 id: 1,
-                text:  `${player.name}, you wake up in a dark empty room tied firmly to a chair.`,
+                text: `${player.name}, you wake up in a dark empty room tied firmly to a chair.`,
                 actions: [
                     {
                         text: "Examine Room",
@@ -45,10 +45,10 @@ function getStoryNodes(player, items, enemies) {
             },
             {
                 id: 2,
-                text: `You notice a broken ${items.glass.name} on the floor next to you.`,
+                text: `You notice a broken ${items.item2.name} on the floor next to you.`,
                 actions: [
                     {
-                        text: `Take ${items.glass.name}`,
+                        text: `Take ${items.item2.name}`,
                     },
                     {
                         text: "Do nothing",
@@ -58,14 +58,14 @@ function getStoryNodes(player, items, enemies) {
             },
             {
                 id: 3,
-                text: "Your body falls on the floor and your hand reaches the broken glass",
+                text: `Your body falls on the floor and your hand reaches the broken ${items.item2.name}`,
                 actions: [
                     {
                         text: "Cut Rope",
                     },
                     {
                         text: "Kill Yourself",
-                        response: "You can't kill yourself tied up in the chair"
+                        response: "You can't kill yourself because you're tied up in the chair"
                     }
                 ]
             },
@@ -84,10 +84,10 @@ function getStoryNodes(player, items, enemies) {
             },
             {
                 id: 5,
-                text: "You see a rusty key in the corner of the room.",
+                text:`You see a rusty ${items.item1.name} in the corner of the room.`,
                 actions: [
                     {
-                        text: "Use Key"
+                        text: `Use the ${items.item1.name}`
                     },
                     {
                         text: "Smash Door",
@@ -97,7 +97,7 @@ function getStoryNodes(player, items, enemies) {
             },
             {
                 id: 6,
-                text: "You have successfully opened the door with the key"
+                text: "You have successfully opened the door!"
             }
         ],
         2: [
@@ -141,11 +141,15 @@ function showStory(roomIndex, storyIndex) {
     storyIndex++;
     currentRoom = rooms[roomIndex];
     const storyNode = storyNodes[currentRoom.id].find(storyNode => storyNode.id === storyIndex);
-    textContainer.innerHTML = `<p>${storyNode.text}</p>`;
+    textContainer.innerHTML = `<p id="story-text">${storyNode.text}</p>`;
     const actions = storyNode.actions;
     if (storyIndex < storyNodes[currentRoom.id].length) {
         for (let i = 0; i < buttons.length; i++) {
+            buttons[i].classList.add("fade");
             buttons[i].innerText = actions[i].text;
+            setTimeout(() => {
+                buttons[i].classList.remove("fade");
+            }, 1000);
             buttons[i].addEventListener("click", () => {
                 if (storyNode.actions[i].hasOwnProperty("response")) {
                     textContainer.innerHTML = `<p>${storyNode.actions[i].response}</p>` 
@@ -173,7 +177,7 @@ function startGame() {
 }
 
 function nextRoom() {
-    setTimeout(function() {
+    setTimeout(() => {
         roomIndex++;
         storyIndex = 0;
         showRoom(roomIndex);
