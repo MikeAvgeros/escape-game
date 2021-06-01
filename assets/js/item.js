56909ef1e0a9aa350d793c17b91ac2032503f3d7
Mike Avgeros <mike.avgeros@gmail.com>
2021-06-01 18:06:14 +0100

    Updated the size of my icons. Removed unused code from item.js

--- assets/js/item.js
+++ assets/js/item.js
@@ -5,14 +5,14 @@ class Item {
         this.name = name;
         this.img = img;
     }
-    showName() {
-        const nameContainer = document.getElementById("item-name");
-        nameContainer.innerHTML = `<p>${this.name}</p>`;
-    }
-    showImage() {
-        const inventory = document.getElementById("inventory-modal");
-        inventory.innerHTML += `<img loading="lazy" id=${this.name} class="inventory-img" src="${this.img}" alt="image of a ${this.name}"></img>`;
-    }
+    // showName() {
+    //     const nameContainer = document.getElementById("item-name");
+    //     nameContainer.innerHTML = `<p>${this.name}</p>`;
+    // }
+    // showImage() {
+    //     const inventory = document.getElementById("inventory-modal");
+    //     inventory.innerHTML += `<img loading="lazy" id=${this.name} class="inventory-img" src="${this.img}" alt="image of a ${this.name}"></img>`;
+    // }
     // deleteImage() {
     //     const thisImage = document.getElementById( `${this.name}`);
     //     thisImage.style.display = "none";
@@ -20,8 +20,8 @@ class Item {
 }
 
 class Weapon extends Item {
-    constructor(name, img, showName, showImage, attack, defense) {
-        super(name, img, showName, showImage);
+    constructor(name, img, attack, defense) {
+        super(name, img);
         this.attack = attack;
         this.defense = defense;
     }
